a62b8860a3b64367b1703188871f9c8bd4bf5315
Lucia <luciamorenonegro@gmail.com>
2020-10-26 20:47:36 -0300

    change style raffles form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,9 @@
-import { html , render } from 'https://unpkg.com/lit-html?module';
-
+import { html, render } from 'https://unpkg.com/lit-html?module';
 
 import { isAuthenticated } from './utils/auth.js';
 import authenticatedApp from './authenticatedApp.js';
 import unauthenticatedApp from './unauthenticatedApp.js';
+// import raffles from './raffles.js';
 
 const renderApp = () => {
   const app = isAuthenticated() ? authenticatedApp : unauthenticatedApp;

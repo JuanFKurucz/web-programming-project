1ee3ad09ab731674329e0337e9cf25902ea3e274
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2020-10-26 20:06:53 -0300

    feat: add login template

---
+++ public/scripts/templates/loginForm.js
@@ -0,0 +1,54 @@
+import { html, nothing } from 'https://unpkg.com/lit-html?module';
+
+import { logIn } from '../services/auth.js';
+
+const loginForm = () => {
+  const error = null;
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    const username = event.target.username.value;
+    const password = event.target.password.value;
+
+    try {
+      await logIn(username, password);
+    } catch (err) {
+      // TODO: Render error.
+    }
+  };
+
+  return html`
+    <div class="container">
+      <section class="login">
+        <h1>Ingresar</h1>
+        <form @submit=${submitHandler}>
+          <div class="form-group username">
+            <label for="username">Usuario</label><br />
+            <input
+              class="ourInput"
+              name="username"
+              type="text"
+              placeholder="Ingresar su usuario"
+            />
+          </div>
+
+          <div class="form-group password">
+            <label for="password">Contraseña</label><br />
+            <input
+              class="ourInput"
+              name="password"
+              type="password"
+              placeholder="Ingresar su contraseña"
+            />
+          </div>
+
+          <button class="loginButton">Ingresar</button>
+          ${error ? html`<p>${error.message}</p>` : nothing}
+        </form>
+      </section>
+    </div>
+  `;
+};
+
+export default loginForm;

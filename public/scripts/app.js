9d5ae60686670903dc948315b09d6712639f93fa
Lucia <luciamorenonegro@gmail.com>
2020-10-28 18:35:34 -0300

    raffles by name form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,7 +3,7 @@ import { html, render } from 'https://unpkg.com/lit-html?module';
 import { isAuthenticated } from './utils/auth.js';
 import authenticatedApp from './authenticatedApp.js';
 import unauthenticatedApp from './unauthenticatedApp.js';
-// import raffles from './raffles.js';
+// import rafflesByNames from './rafflesByNames.js';
 
 const renderApp = () => {
   const app = isAuthenticated() ? authenticatedApp : unauthenticatedApp;

6f5659d8ab04d80c3538fb30f051fa3010f6e0af
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2020-11-09 23:34:30 -0300

    feat: endpoint to list created raffles

--- functions/raffles/methods/index.js
+++ functions/raffles/methods/index.js
@@ -1,7 +1,14 @@
+const getRaffles = require('./getRaffles');
 const createRaffle = require('./createRaffle');
 
 // List of endpoints from users with their respective methods and options
 module.exports = {
+  GET: {
+    method: getRaffles,
+    options: {
+      loginRequired: true,
+    },
+  },
   POST: {
     method: createRaffle,
     options: {

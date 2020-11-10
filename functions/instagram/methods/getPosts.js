3a63772eb7b7b2a073a9f4ec7e00a7b7944ad400
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2020-11-09 23:29:59 -0300

    chore: rename facebook library api

--- functions/instagram/methods/getPosts.js
+++ functions/instagram/methods/getPosts.js
@@ -2,7 +2,7 @@ const {
   formatError,
   formatOutput,
 } = require('../../../libs/utils/formatOutput');
-const api = require('../../../libs/clients/facebook');
+const facebookApi = require('../../../libs/clients/facebook');
 
 const getUser = async (event, user) => {
   if (!user.accessToken || user.accessToken.length === 0) {
@@ -10,15 +10,15 @@ const getUser = async (event, user) => {
   }
   let posts = [];
   try {
-    api.setAccessToken(user.accessToken);
-    const pages = await api.getFacebookPages();
+    facebookApi.setAccessToken(user.accessToken);
+    const pages = await facebookApi.getFacebookPages();
     const instagramPages = [];
     for (let page = 0; page < pages.length; page += 1) {
-      instagramPages.push(api.getInstagramId(pages[page]));
+      instagramPages.push(facebookApi.getInstagramId(pages[page]));
     }
     const instagramIds = await Promise.all(instagramPages);
     for (let i = 0; i < instagramIds.length; i += 1) {
-      posts = [...posts, api.getInstagramPosts(instagramIds[i])];
+      posts = [...posts, facebookApi.getInstagramPosts(instagramIds[i])];
     }
     posts = await Promise.all(posts);
     return formatOutput(200, posts[0]);

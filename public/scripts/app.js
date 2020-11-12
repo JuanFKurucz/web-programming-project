a1ac0af36a7c74c8bbe4f2b9ae1bf25c0c05ac71
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2020-11-11 23:37:22 -0300

    feat: use navigate to reroute clicks to a elements

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,12 @@
-import { html, render } from 'https://unpkg.com/lit-html?module';
+import { render } from 'https://unpkg.com/lit-html?module';
 
 import { isAuthenticated } from './utils/auth.js';
 import authenticatedApp from './authenticatedApp.js';
 import unauthenticatedApp from './unauthenticatedApp.js';
-// import rafflesByNames from './rafflesByNames.js';
 
 const renderApp = () => {
   const app = isAuthenticated() ? authenticatedApp : unauthenticatedApp;
-  render(html`${app()}`, document.body);
+  render(app(), document.body);
 };
 
 window.addEventListener('nav', renderApp);
--- public/scripts/unauthenticatedApp.js
+++ public/scripts/unauthenticatedApp.js
@@ -1,7 +1,13 @@
 import { html } from 'https://unpkg.com/lit-html?module';
 
 import loginForm from './templates/loginForm.js';
+import router from './templates/router.js';
 
-const unauthenticatedApp = () => html` ${loginForm()} `;
+const routes = {
+  '/': loginForm,
+  '/login': loginForm,
+};
+
+const unauthenticatedApp = () => html`${router(routes)}`;
 
 export default unauthenticatedApp;

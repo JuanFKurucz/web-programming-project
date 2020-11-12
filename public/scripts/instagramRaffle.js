b0c106d9a14a42d999751fc57a37a8958f460eba
Lucia <luciamorenonegro@gmail.com>
2020-11-12 18:13:23 -0300

    instagramRaffle router

--- public/scripts/instagramRaffle.js
+++ public/scripts/instagramRaffle.js
@@ -2,6 +2,12 @@ import { html } from 'https://unpkg.com/lit-html?module';
 
 import instagramRaffleForm from './templates/instagramRaffleForm.js';
 
-const instagramRaffle = () => html` ${instagramRaffleForm()} `;
+import router from './templates/router.js';
+
+const routes = {
+  '/instagramRaffle': instagramRaffleForm,
+};
+
+const instagramRaffle = () => html`${router(routes)}`;
 
 export default instagramRaffle;

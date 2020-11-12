6ffc5fa2f8f719c392a0ec3e5bfec06d8bf81dd0
Lucia <luciamorenonegro@gmail.com>
2020-11-12 19:41:38 -0300

    change authenticatedApp

--- public/scripts/authenticatedApp.js
+++ public/scripts/authenticatedApp.js
@@ -1,12 +1,17 @@
 import { html } from 'https://unpkg.com/lit-html?module';
 
 import { logOut } from './services/auth.js';
+import rafflesForm from './templates/rafflesForm.js';
+
 import router from './templates/router.js';
 
 const routes = {
-  '/logOut': logOut,
+  '/rafflesForm': rafflesForm,
 };
 
-const authenticatedApp = () => html`${router(routes)}`;
+const authenticatedApp = () => html`
+  ${rafflesForm()} ${router(routes)}
+  <button @click=${logOut} type="button">Log Out</button>
+`;
 
 export default authenticatedApp;

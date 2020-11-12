420ece2994053ba93037fe86bcee42e989b12abd
Lucia <luciamorenonegro@gmail.com>
2020-11-12 18:11:05 -0300

    raffles router

--- public/scripts/raffles.js
+++ public/scripts/raffles.js
@@ -2,6 +2,12 @@ import { html } from 'https://unpkg.com/lit-html?module';
 
 import rafflesForm from './templates/rafflesForm.js';
 
-const raffles = () => html` ${rafflesForm()} `;
+import router from './templates/router.js';
+
+const routes = {
+  '/raffles': rafflesForm,
+};
+
+const raffles = () => html`${router(routes)}`;
 
 export default raffles;

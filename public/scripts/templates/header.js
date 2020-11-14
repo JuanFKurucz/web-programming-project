9bfbbbf013ad9a9a95480c400307b9fb7afb3090
Lucia <luciamorenonegro@gmail.com>
2020-11-14 15:53:08 -0300

    salir

--- public/scripts/templates/header.js
+++ public/scripts/templates/header.js
@@ -1,6 +1,22 @@
 import { html } from 'https://unpkg.com/lit-html?module';
+import { logOut } from '../services/auth.js';
+
+const submitHandlerLogOut = async (event) => {
+  event.preventDefault();
+  try {
+    await logOut();
+  } catch (err) {
+    throw new Error('Oops! Something went wrong...');
+  }
+};
 
 const header = () => {
-  return html` <header></header> `;
+  return html`
+    <header>
+      <button type="button" @click=${submitHandlerLogOut}>
+        Salir
+      </button>
+    </header>
+  `;
 };
 export default header;
